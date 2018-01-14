3382d73ea72ec40450edb392a8f20aa39e42011f
KarateJB <ppippenkimo@gmail.con>
2018-01-14 09:05:02 +0800

    Add upload view

---
+++ CSharp/DP.Website/Controllers/UploadController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DP.Website.Models;
+using DP.Website.Domain;
+
+namespace DP.Website.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string fileType)
+        {
+            ViewBag.fileType=fileType;
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
